a95623c3cd04e8c79107f0a5d62959d6413f85f5
akumarm23 <ak.muniswamy2023@gmail.com>
2025-01-23 18:19:38 -0500

    Add stock lookup form and regular stock lookup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
 
 end
